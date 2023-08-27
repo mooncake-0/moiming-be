8e417ce2402794b8ec941d76b8a38cd057a12844
wrock.kang <a@moiming.net>
2023-08-27 22:32:27 +0900

    config edit & merge ready 2

--- src/test/java/com/peoplein/moiming/MoimingApplicationTests.java
+++ src/test/java/com/peoplein/moiming/MoimingApplicationTests.java
@@ -3,6 +3,7 @@ package com.peoplein.moiming;
 import com.peoplein.moiming.domain.BaseEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class MoimingApplicationTests  {
